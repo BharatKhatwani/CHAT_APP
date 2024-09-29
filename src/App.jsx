68cd4cc56295df75de7b76f50101635d3cb45f35
BharatKhatwani <bharatkhatwani06@gmail.com>
2024-09-30 01:49:19 +0530

    file_path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './config/firebase'; // Correct capitalization of 'config'
+import { auth } from './Config/firebase'; // Correct capitalization of 'config'
  // Firebase authentication
 
 const App = () => {
